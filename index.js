d70ab709e8f50127390f2ca28c2de2e00830c036
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-25 18:05:02 -0700

    required packages and an array crated

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-
+// require the files 
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
+
+const {
+  generateEngineerCard,
+  generateManagerCard,
+  generateInternCard,
+  baseHtml
+} = require("./src/htmlGen");
+// an empty array to push all the answers to 
+const teamMemberHtmlArr = [];
