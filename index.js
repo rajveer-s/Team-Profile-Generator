e419f9411d700ba3cf4b24d08992f4f55f01b611
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-25 18:02:42 -0700

    Created all the required packages in index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// Get all the required packages 
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
