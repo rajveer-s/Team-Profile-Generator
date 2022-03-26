5778a4db4fca54e270e40c8cc951691a8ab899ad
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-25 17:56:14 -0700

    Intern card created htmlgen.js

--- src/htmlGen.js
+++ src/htmlGen.js
@@ -1,3 +1,4 @@
+// creatinf manager card to display in html 
 const generateManagerCard = (manager) => {
   return `
       <div class="card">
@@ -9,6 +10,24 @@ const generateManagerCard = (manager) => {
             <li class="list-group-item">Email: <a href="mailto:${manager.getEmail().toLowerCase()}">${manager.getEmail().toLowerCase()}</a>
             </li>
             <li class="list-group-item">Phone: ${manager.getOfficeNumber()}</li>
+          </ul>
+        </div>
+      </div>
+  `
+}
+
+// intern card to display in html 
+const generateInternCard = (intern) => {
+  return `
+      <div class="card">
+        <h3>${intern.getName().toUpperCase()}</h3>
+        <p><i class="fa-solid fa-user-graduate"></i>${intern.getRole()}</p>
+        <div class="card-body">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail().toLowerCase()}">${intern.getEmail().toLowerCase()}</a>
+            </li>
+            <li class="list-group-item">School: ${intern.getSchool()}</li>
           </ul>
         </div>
       </div>
