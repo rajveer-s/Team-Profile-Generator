adde17792e4df823c5649a2b994927faa93aab5b
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-25 17:58:35 -0700

    Html Base created htmlgen.js

--- src/htmlGen.js
+++ src/htmlGen.js
@@ -52,3 +52,89 @@ const generateEngineerCard = (engineer) => {
       </div>
   `
 }
+
+// html to display the info ojn the page                 
+const baseHtml = (htmlArr) => `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/f5f4ff2f36.js" crossorigin="anonymous"></script>
+  <title>Team Profile Generator</title>
+  <style>
+*{
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+body a{
+  outline: none;
+  text-decoration: none;
+  list-style: none;
+}
+header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(185, 64, 64);
+  color: #fff;
+  height: 20vh;
+}
+.heading{
+  font-size: 2rem;
+  font-weight: 700;
+}
+#container{
+  display: flex;
+}
+.card-deck {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 5rem;
+  flex-wrap: wrap;  
+}
+.card{
+  height: 24rem;
+  width: 18rem;
+  margin: 2rem;
+}
+h3{
+  background-color: cornflowerblue;
+  padding: 0 1.4rem;
+  margin: 0;
+  font-size: 35px;
+}
+p {
+  background-color: cornflowerblue;
+  font-size: 28px;
+  padding: .4rem 1rem;
+}
+
+p i {
+  padding-right: .9rem;
+  font-size: 2rem;
+}
+</style>
+
+</head>
+
+<body>
+  <header>
+    <div class="jumbotron jumbotron-fluid">
+        <h1 class="display-4 heading">My Team</h1>
+    </div>
+  </header>
+  <div id="container">
+    <div class="card-deck">
+        ${htmlArr.join(' ')}
+    </div>
+  </div>
+</body>
+
+</html>`
