cd5a5825b7e27fa78aa174bc557de48a282aa74b
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-25 18:07:32 -0700

    init function with manager questions created

--- index.js
+++ index.js
@@ -16,3 +16,82 @@ const {
 } = require("./src/htmlGen");
 // an empty array to push all the answers to 
 const teamMemberHtmlArr = [];
+
+
+function init() {
+  function managerCreate() {
+    inquirer.prompt([{
+      // confirms if its the manager or not 
+      name: 'check',
+      type: 'confirm',
+      message: 'Are you the Manager ?',
+
+    },
+    {
+      name: 'name',
+      message: 'what is your Name ?',
+      when: (answers) => {
+        if (answers.check) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    {
+      name: 'email',
+      when: (answers) => {
+        if (answers.check) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      message: 'what is your email ?',
+      validate: function (email) {  //validates that only email syntax is entered in
+
+        const valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+        if (valid) {
+          return true;
+        } else {
+         
+          return "Please enter a valid email";
+        }
+      }
+    },
+    {
+      name: 'id',
+      message: 'what is your id ?',
+      when: (answers) => {
+        if (answers.check) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    {
+      name: 'officeNumber',
+      message: 'what is your Office Number ?',
+      when: (answers) => {
+        if (answers.check) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+    },
+    ])
+      .then(({ name, id, email, officeNumber, check }) => {
+        if (!check) {
+          console.log("go get your manager")
+          return setTimeout(() => {
+            managerCreate()
+          }, 1000);
+        }
+        const manager = new Manager(name, id, email, officeNumber)
+        teamMemberHtmlArr.push(generateManagerCard(manager))
+        mainMenu()
+      })
+  }
